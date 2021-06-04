35df601a9165f41a29af1326ef2a6f8134d934a5
Kiet Pham <kiet.pham@c2c-techhub.io>
2021-06-04 16:13:37 +0700

    add product and category api

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -16,10 +16,10 @@ axios.interceptors.request.use(function (config) {
 });
 
 // Add a response interceptor
-axios.interceptors.response.use(function (response) {
+axiosClient.interceptors.response.use(function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
-    return response;
+    return response.data;
 }, function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
